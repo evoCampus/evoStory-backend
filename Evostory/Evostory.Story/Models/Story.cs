30aff92d97a8b120f63d890e7b71718266ce220b
dvd2001 <bedavid2001@gmail.com>
2025-03-25 20:00:33 +0100

    Making changes based on todos

---
+++ Evostory/Evostory.Story/Models/Story.cs
@@ -0,0 +1,11 @@
+﻿namespace Evostory.Story.Models
+{
+    public class Story
+    {
+        public Guid StoryId { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Scene> Scenes { get; set; }
+        public Guid StartingSceneId { get; set; }
+
+    }
+}
