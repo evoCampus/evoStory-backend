2e9cbb42cc1ffb7933a3b24cecd4bf1cd49d71e2
Balogh Dávid <bedavid2001@gmail.com>
2025-05-12 14:30:56 +0200

    Implementing DTO Conversion Service (#5)
    * Implementing DTO Conversion Service
    
    * Fixing compile time errors
    
    * Using DTOConversionService for every model to dto conversion, and adding a new dependency injection for the service

---
+++ Evostory/EvoStory.BackendAPI/Services/DTOConversionService.cs
@@ -0,0 +1,43 @@
+﻿using Evostory.Story.Models;
+using EvoStory.BackendAPI.DTO;
+
+namespace EvoStory.BackendAPI.Services
+{
+    public class DTOConversionService: IDTOConversionService
+    {
+        public StoryDTO ConvertStoryToStoryDTO(Story story)
+        {
+            return new StoryDTO
+            {
+                Id = story.Id,
+                Title = story.Title,
+                StartingSceneId = story.StartingSceneId,
+                Scenes = story.Scenes.Select(s => ConvertSceneToSceneDTO(s)),
+            };
+        }
+        public SceneDTO ConvertSceneToSceneDTO(Scene scene)
+        {
+            return new SceneDTO
+            {
+                Id = scene.Id,
+                Content = new ContentDTO
+                {
+                    Id = scene.Content.Id,
+                    Text = scene.Content.Text,
+                    ImageId = scene.Content.ImageId,
+                    SoundId = scene.Content.SoundId
+                },
+                Choices = scene.Choices.Select(c => ConvertChoiceToChoiceDTO(c))
+            };
+        }
+        public ChoiceDTO ConvertChoiceToChoiceDTO(Choice choice)
+        {
+            return new ChoiceDTO
+            {
+                Id = choice.Id,
+                ChoiceText = choice.ChoiceText,
+                NextSceneId = choice.NextSceneId
+            };
+        }
+    }
+}
---
+++ Evostory/EvoStory.BackendAPI/Services/IDTOConversionService.cs
@@ -0,0 +1,13 @@
+﻿using Evostory.Story.Models;
+using EvoStory.BackendAPI.DTO;
+
+namespace EvoStory.BackendAPI.Services
+{
+    public interface IDTOConversionService
+    {
+        public StoryDTO ConvertStoryToStoryDTO(Story story);
+        public SceneDTO ConvertSceneToSceneDTO(Scene scene);
+        public ChoiceDTO ConvertChoiceToChoiceDTO(Choice choice);
+
+    }
+}
