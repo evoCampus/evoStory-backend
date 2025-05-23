b7ac248227428ff817411d6b7ca6483b13159988
Bolyki Balázs <bbalage0@gmail.com>
2025-05-23 16:43:24 +0200

    Added some unit tests during campus session.

---
+++ Evostory/EvoStory.BackendAPI.uTest/ChoiceServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using Evostory.Story.Models;
+using EvoStory.BackendAPI.DTO;
+using EvoStory.BackendAPI.Repository;
+using EvoStory.BackendAPI.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoStory.BackendAPI.uTest
+{
+    class ChoiceServiceTest
+    {
+        [Test]
+        public void CreateChoice_ValidCreateChoiceDTO_ChoiceIsAddToRepository()
+        {
+            // Arrange
+            var sceneId = Guid.NewGuid();
+            const string choiceText = "Some choice text.";
+            var nextSceneId = Guid.NewGuid();
+            var choiceId = Guid.NewGuid();
+            var createChoiceDTO = new CreateChoiceDTO()
+            {
+                SceneId = sceneId,
+                ChoiceText = choiceText,
+                NextSceneId = nextSceneId
+            };
+            var choice = new Choice()
+            {
+                Id = choiceId,
+                ChoiceText = choiceText,
+                NextSceneId = nextSceneId
+            };
+            var expectedChoiceDTO = new ChoiceDTO()
+            {
+                Id = choiceId,
+                ChoiceText = choiceText,
+                NextSceneId = nextSceneId
+            };
+            var choiceRepositoryMock = new Mock<IChoiceRepository>();
+            choiceRepositoryMock.Setup(m => m.CreateChoice(It.IsAny<Choice>()))
+                .Returns(choice);
+            var dtoConversionServiceMock = new Mock<IDTOConversionService>();
+            dtoConversionServiceMock.Setup(m => m.ConvertChoiceToChoiceDTO(It.IsAny<Choice>()))
+                .Returns(expectedChoiceDTO);
+
+            var sut = new ChoiceService(choiceRepositoryMock.Object, dtoConversionServiceMock.Object);
+
+            // Act
+            var actualChoiceDTO = sut.CreateChoice(createChoiceDTO);
+
+            // Assert
+            Assert.That(actualChoiceDTO.Id, Is.EqualTo(expectedChoiceDTO.Id));
+            Assert.That(actualChoiceDTO.ChoiceText, Is.EqualTo(expectedChoiceDTO.ChoiceText));
+            Assert.That(actualChoiceDTO.NextSceneId, Is.EqualTo(expectedChoiceDTO.NextSceneId));
+        }
+    }
+}
---
+++ Evostory/EvoStory.BackendAPI.uTest/Services/DTOConversionServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Evostory.Story.Models;
+using EvoStory.BackendAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoStory.BackendAPI.uTest.Services
+{
+    internal class DTOConversionServiceTest
+    {
+        [Test]
+        public void ConvertChoiceToChoiceDTO_CompleteChoice_ChoiceDTOCreatedWithEqualValues()
+        {
+            // Arrange = Given
+            Guid id = Guid.NewGuid();
+            const string choiceText = "Some choice text.";
+            Guid nextSceneId = Guid.NewGuid();
+            var choice = new Choice()
+            {
+                Id = id,
+                ChoiceText = choiceText,
+                NextSceneId = nextSceneId
+            };
+            var sut = new DTOConversionService();
+
+            // Act = When
+            var choiceDTO = sut.ConvertChoiceToChoiceDTO(choice);
+
+            // Assert = Then
+            Assert.That(choiceDTO.Id, Is.EqualTo(id));
+            Assert.That(choiceDTO.ChoiceText, Is.EqualTo(choiceText));
+            Assert.That(choiceDTO.NextSceneId, Is.EqualTo(nextSceneId));
+        }
+    }
+}
